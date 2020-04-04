0582bfdbdae8a195068d6d54f4e24cde872acb41
ArturZybin <artur.zybin.work@gmail.com>
2020-04-04 12:05:15 +0700

    Updates

--- precache-manifest.1549b99cdcce537b412c58087c0b76f6.js
+++ precache-manifest.1549b99cdcce537b412c58087c0b76f6.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "74094393abfc6dc3da76c715e53146c6",
+    "revision": "d4736a8f1ff1f87bd4baa10a02a53bb9",
     "url": "./index.html"
   },
   {
-    "revision": "8aadcae570f536956878",
+    "revision": "49150972ea170d42b65d",
     "url": "./static/css/2.764ccc25.chunk.css"
   },
   {
-    "revision": "01129516fdd527a01b1b",
-    "url": "./static/css/main.da835e92.chunk.css"
+    "revision": "7262a0212c556c7d0073",
+    "url": "./static/css/main.9698b496.chunk.css"
   },
   {
-    "revision": "8aadcae570f536956878",
-    "url": "./static/js/2.aafc62d1.chunk.js"
+    "revision": "49150972ea170d42b65d",
+    "url": "./static/js/2.e8904199.chunk.js"
   },
   {
     "revision": "f9416c2fa5dace694260608407c8cf2e",
-    "url": "./static/js/2.aafc62d1.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.e8904199.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "01129516fdd527a01b1b",
-    "url": "./static/js/main.7e07bb57.chunk.js"
+    "revision": "7262a0212c556c7d0073",
+    "url": "./static/js/main.391a9a55.chunk.js"
   },
   {
     "revision": "92b501c2400ffa810932",
